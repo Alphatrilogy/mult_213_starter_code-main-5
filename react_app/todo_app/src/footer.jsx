4cac0d60b92e3a5744050bf07b9b522a58c4dfca
Cade Proctor <proctor3756@saskpolytech.ca>
2025-12-05 10:17:25 -0600

    copied the header.jsx to footer and tweaked it and added a css for it too

---
+++ react_app/todo_app/src/footer.jsx
@@ -0,0 +1,11 @@
+import './footer.css';
+
+// Add a new component for the Header
+export function footer(endingMessage) {
+    return (
+    <footer>
+      <p>{message}</p>
+      <p>Copyright © 2023 My Website. All rights reserved.</p>
+    </footer>
+  );
+}
